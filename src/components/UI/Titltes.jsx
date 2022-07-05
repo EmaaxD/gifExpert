251e916f0533a03a9c67507aeb780bf9af8843de
Emanuel Mamani <emanuelmamani@uneio.com>
2022-07-05 10:08:17 -0300

    fixed

--- src/components/UI/Titltes.jsx
+++ src/components/UI/Titltes.jsx
@@ -5,17 +5,11 @@ export const TitlteApp = () => {
   return (
     <>
       <Fade top>
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col py-5 px-5 text-center">
-            <h1 className="text-3xl font-bold">Git Expert App</h1>
-            <h5 className="text-black text-sm font-medium">
-              Todos los gifs que quieras!!!
-            </h5>
-          </div>
-
-          <div>
-            <TrashIcon className="w-5 h-5 text-red-500" />
-          </div>
+        <div className="flex flex-col py-5 px-5 text-center">
+          <h1 className="text-3xl font-bold">Git Expert App</h1>
+          <h5 className="text-black text-sm font-medium">
+            Todos los gifs que quieras!!!
+          </h5>
         </div>
       </Fade>
     </>
@@ -26,9 +20,14 @@ export const TitleCardGif = ({ text }) => {
   return (
     <>
       <Fade right>
-        <h4 className="text-black text-2xl md:text-lg font-semibold first-letter:uppercase">
-          {text}
-        </h4>
+        <div className="flex justify-between items-center">
+          <h4 className="text-black text-2xl md:text-lg font-semibold first-letter:uppercase">
+            {text}
+          </h4>
+          <button type="button">
+            <TrashIcon className="w-5 h-5 text-red-500" />
+          </button>
+        </div>
       </Fade>
     </>
   );
