1c1c4f75d608908fec81d505dde2249cda8089f0
Emanuel Mamani <emanuelmamani@uneio.com>
2022-07-05 10:02:27 -0300

    fixed add delete gif group

--- src/components/UI/Titltes.jsx
+++ src/components/UI/Titltes.jsx
@@ -1,14 +1,21 @@
+import { TrashIcon } from "@heroicons/react/outline";
 import Fade from "react-reveal/Fade";
 
 export const TitlteApp = () => {
   return (
     <>
       <Fade top>
-        <div className="flex flex-col py-5 px-5 text-center">
-          <h1 className="text-3xl font-bold">Git Expert App</h1>
-          <h5 className="text-black text-sm font-medium">
-            Todos los gifs que quieras!!!
-          </h5>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col py-5 px-5 text-center">
+            <h1 className="text-3xl font-bold">Git Expert App</h1>
+            <h5 className="text-black text-sm font-medium">
+              Todos los gifs que quieras!!!
+            </h5>
+          </div>
+
+          <div>
+            <TrashIcon className="w-5 h-5 text-red-500" />
+          </div>
         </div>
       </Fade>
     </>
